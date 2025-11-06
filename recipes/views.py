40e085d9c5c74c6d14802761ae8ff800abd082ac
lucashaiter <lucas.leoni.haiter@gmail.com>
2025-11-06 20:30:07 -0300

    fontawesome and header

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # HTTP Request
 def home_view(request):
     # return HTTP Response
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas'
     })
